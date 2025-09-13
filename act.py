a9378090af4cf4dfb2721c78acd7c552ef8e6188
Ariel <114245987+yelyail@users.noreply.github.com>
2025-09-13 10:20:22 +0800

    First Activity
    Introduces a Person class with name, address, and gender attributes. Includes instantiation of a Person object and prints its properties.

---
+++ act.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, address, gender):
+        self.name = name
+        self.address = address
+        self.gender = gender
+
+#objects of the class
+person1 =Person("Yel", "Davao City", "F")
+
+person1.name
+person1.address 
+person1.gender
+
+print(person1.name)
+print(person1.address)
+print(person1.gender)
